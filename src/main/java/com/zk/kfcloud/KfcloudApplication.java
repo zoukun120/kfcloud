78a6e51bb878128be3af89deecef9bd8dc3859e3
zoukun120 <18367053731@163.com>
2018-03-30 17:10:02 +0800

    frist commit

---
+++ src/main/java/com/zk/kfcloud/KfcloudApplication.java
@@ -0,0 +1,12 @@
+package com.zk.kfcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KfcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KfcloudApplication.class, args);
+	}
+}
