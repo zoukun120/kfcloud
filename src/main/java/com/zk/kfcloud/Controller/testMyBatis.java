8bfac41de3632a35785e03cc1a11c67ed3af9ebd
zoukun120 <18367053731@163.com>
2018-04-01 23:58:01 +0800

    日志

--- src/main/java/com/zk/kfcloud/Controller/testMyBatis.java
+++ src/main/java/com/zk/kfcloud/Controller/testMyBatis.java
@@ -3,12 +3,16 @@ package com.zk.kfcloud.Controller;
 
 import com.zk.kfcloud.Dao.UserMapper;
 import com.zk.kfcloud.Entity.web.User;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class testMyBatis {
 
@@ -18,7 +22,10 @@ public class testMyBatis {
     @GetMapping("/findAll")
     List<User> findall(){
         List<User> allUsers = userMapper.findAllUsers();
-        System.out.println(allUsers);
+        log.info("info");
+        log.debug("debug");
+        log.warn("warn");
+        log.error("error");
         return allUsers;
     }
 }
