3114e41828877ae91aa7ce912ba1f5caf3a38441
zoukun120 <18367053731@163.com>
2018-04-02 00:19:11 +0800

    权限管理

---
+++ src/main/java/com/zk/kfcloud/Entity/web/Role.java
@@ -0,0 +1,33 @@
+package com.zk.kfcloud.Entity.web;
+
+public class Role {
+    private Integer roleId;
+
+    private String roleName;
+
+    private String rights;
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName == null ? null : roleName.trim();
+    }
+
+    public String getRights() {
+        return rights;
+    }
+
+    public void setRights(String rights) {
+        this.rights = rights == null ? null : rights.trim();
+    }
+}
