81f9217fad60efc49800a982290d0e7e6f9ac535
zoukun120 <18367053731@163.com>
2018-04-14 09:44:24 +0800

    首页样式调整

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -89,69 +89,69 @@ $('.wclose').click(function(e){
 
 /* Calendar */
 
-  $(document).ready(function() {
-  
-    var date = new Date();
-    var d = date.getDate();
-    var m = date.getMonth();
-    var y = date.getFullYear();
-    
-    $('#calendar').fullCalendar({
-      header: {
-        left: 'prev',
-        center: 'title',
-        right: 'month,agendaWeek,agendaDay,next'
-      },
-      editable: true,
-      events: [
-        {
-          title: 'All Day Event',
-          start: new Date(y, m, 1)
-        },
-        {
-          title: 'Long Event',
-          start: new Date(y, m, d-5),
-          end: new Date(y, m, d-2)
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: new Date(y, m, d-3, 16, 0),
-          allDay: false
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: new Date(y, m, d+4, 16, 0),
-          allDay: false
-        },
-        {
-          title: 'Meeting',
-          start: new Date(y, m, d, 10, 30),
-          allDay: false
-        },
-        {
-          title: 'Lunch',
-          start: new Date(y, m, d, 12, 0),
-          end: new Date(y, m, d, 14, 0),
-          allDay: false
-        },
-        {
-          title: 'Birthday Party',
-          start: new Date(y, m, d+1, 19, 0),
-          end: new Date(y, m, d+1, 22, 30),
-          allDay: false
-        },
-        {
-          title: 'Click for Google',
-          start: new Date(y, m, 28),
-          end: new Date(y, m, 29),
-          url: 'http://google.com/'
-        }
-      ]
-    });
-    
-  });
+  // $(document).ready(function() {
+  //
+  //   var date = new Date();
+  //   var d = date.getDate();
+  //   var m = date.getMonth();
+  //   var y = date.getFullYear();
+  //
+  //   $('#calendar').fullCalendar({
+  //     header: {
+  //       left: 'prev',
+  //       center: 'title',
+  //       right: 'month,agendaWeek,agendaDay,next'
+  //     },
+  //     editable: true,
+  //     events: [
+  //       {
+  //         title: 'All Day Event',
+  //         start: new Date(y, m, 1)
+  //       },
+  //       {
+  //         title: 'Long Event',
+  //         start: new Date(y, m, d-5),
+  //         end: new Date(y, m, d-2)
+  //       },
+  //       {
+  //         id: 999,
+  //         title: 'Repeating Event',
+  //         start: new Date(y, m, d-3, 16, 0),
+  //         allDay: false
+  //       },
+  //       {
+  //         id: 999,
+  //         title: 'Repeating Event',
+  //         start: new Date(y, m, d+4, 16, 0),
+  //         allDay: false
+  //       },
+  //       {
+  //         title: 'Meeting',
+  //         start: new Date(y, m, d, 10, 30),
+  //         allDay: false
+  //       },
+  //       {
+  //         title: 'Lunch',
+  //         start: new Date(y, m, d, 12, 0),
+  //         end: new Date(y, m, d, 14, 0),
+  //         allDay: false
+  //       },
+  //       {
+  //         title: 'Birthday Party',
+  //         start: new Date(y, m, d+1, 19, 0),
+  //         end: new Date(y, m, d+1, 22, 30),
+  //         allDay: false
+  //       },
+  //       {
+  //         title: 'Click for Google',
+  //         start: new Date(y, m, 28),
+  //         end: new Date(y, m, 29),
+  //         url: 'http://google.com/'
+  //       }
+  //     ]
+  //   });
+  //
+  // });
 
 /* Progressbar animation */
 
@@ -182,46 +182,46 @@ $('.wclose').click(function(e){
     },600);
 
 /* Slider */
-
-    $(function() {
-        // Horizontal slider
-        $( "#master1, #master2" ).slider({
-            value: 60,
-            orientation: "horizontal",
-            range: "min",
-            animate: true
-        });
-
-        $( "#master4, #master3" ).slider({
-            value: 80,
-            orientation: "horizontal",
-            range: "min",
-            animate: true
-        });        
-
-        $("#master5, #master6").slider({
-            range: true,
-            min: 0,
-            max: 400,
-            values: [ 75, 200 ],
-            slide: function( event, ui ) {
-                $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-            }
-        });
-
-
-        // Vertical slider 
-        $( "#eq > span" ).each(function() {
-            // read initial values from markup and remove that
-            var value = parseInt( $( this ).text(), 10 );
-            $( this ).empty().slider({
-                value: value,
-                range: "min",
-                animate: true,
-                orientation: "vertical"
-            });
-        });
-    });
+    //
+    // $(function() {
+    //     // Horizontal slider
+    //     $( "#master1, #master2" ).slider({
+    //         value: 60,
+    //         orientation: "horizontal",
+    //         range: "min",
+    //         animate: true
+    //     });
+    //
+    //     $( "#master4, #master3" ).slider({
+    //         value: 80,
+    //         orientation: "horizontal",
+    //         range: "min",
+    //         animate: true
+    //     });
+    //
+    //     $("#master5, #master6").slider({
+    //         range: true,
+    //         min: 0,
+    //         max: 400,
+    //         values: [ 75, 200 ],
+    //         slide: function( event, ui ) {
+    //             $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+    //         }
+    //     });
+    //
+    //
+    //     // Vertical slider
+    //     $( "#eq > span" ).each(function() {
+    //         // read initial values from markup and remove that
+    //         var value = parseInt( $( this ).text(), 10 );
+    //         $( this ).empty().slider({
+    //             value: value,
+    //             range: "min",
+    //             animate: true,
+    //             orientation: "vertical"
+    //         });
+    //     });
+    // });
 
 
 
@@ -301,27 +301,27 @@ $(document).ready(function() {
 
 /* Date picker */
 
-  $(function() {
-    $('#datetimepicker1').datetimepicker({
-      pickTime: false
-    });
-  });
-
-   $(function() {
-    $('#datetimepicker2').datetimepicker({
-      pickDate: false
-    });
-  });
+  // $(function() {
+  //   $('#datetimepicker1').datetimepicker({
+  //     pickTime: false
+  //   });
+  // });
+  //
+  //  $(function() {
+  //   $('#datetimepicker2').datetimepicker({
+  //     pickDate: false
+  //   });
+  // });
 
 
 
 
 /* CL Editor */
 
-$(".cleditor").cleditor({
-    width: "auto",
-    height: "auto"
-});
+// $(".cleditor").cleditor({
+//     width: "auto",
+//     height: "auto"
+// });
 
 /* Modal fix */
 
@@ -329,6 +329,6 @@ $('.modal').appendTo($('body'));
 
 /* Pretty Photo for Gallery*/
 
-jQuery("a[class^='prettyPhoto']").prettyPhoto({
-overlay_gallery: false, social_tools: false
-});
+// jQuery("a[class^='prettyPhoto']").prettyPhoto({
+// overlay_gallery: false, social_tools: false
+// });
